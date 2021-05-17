27b318ebc96098113b248b9e78d4daf64e918628
Nikita <nikjeg92@gmail.dom>
2021-05-17 22:29:12 +0300

    Praktika chast 2 urok 15

--- script.js
+++ script.js
@@ -7,12 +7,25 @@ let personalMovieDB = {
   privat: false,
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-  b = prompt("На сколько оцените его?", ""),
-  c = prompt("Один из последних просмотренных фильмов?", ""),
-  d = prompt("На сколько оцените его?", "");
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последних просмотренных фильмов?", ""),
+    b = prompt("На сколько оцените его?", "");
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("done");
+  } else {
+    console.log("error");
+    i--;
+  }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
+if (personalMovieDB.count < 10) {
+  console.log("Мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log("Достаточно фильмов");
+} else if (personalMovieDB.count >= 30) {
+  console.log("Киноман");
+} else {
+  console.log("Произошла Обишка");
+}
 console.log(personalMovieDB);
